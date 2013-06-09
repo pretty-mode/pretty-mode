d7bd6c7ebe50f83d82af413e0d53f5b0de14acf6
Grant Rettke <gcr@stoneisle.com>
2013-06-08 21:19:22 -0500

    Added fixtures for all of the documented symbols supported for scheme either via scheme or the lispy list in PMP.

---
+++ fixtures/fixture.scm
@@ -0,0 +1,24 @@
+#!r6rs
+
+(import (rnrs base (6)))
+
+; Logic
+
+(assert 'not) ; 00AC ¬ NOT SIGN
+(assert 'and) ; 2227 ∧ LOGICAL AND
+(assert 'or) ; 2228 ∨ LOGICAL OR
+
+; Arrows
+
+(assert '<-) ; 2190 ← LEFTWARDS ARROW
+(assert '->) ; 2192 → RIGHTWARDS ARROW
+(assert '->>) ; 21A0 ↠ RIGHTWARDS TWO HEADED ARROW
+(assert '=>) ; 21D2 ⇒ RIGHTWARDS DOUBLE ARROW
+
+; Equality
+
+(assert '!=) ; 2260 ≠ NOT EQUAL TO
+(assert '...) ; 2026 … HORIZONTAL ELLIPSIS
+(assert 'null) ; 2205 ∅ EMPTY SET "null"
+(assert ''()) ; 2205 ∅ EMPTY SET "'()"
+(assert 'empty) ; 2205 ∅ EMPTY SET  "empty"

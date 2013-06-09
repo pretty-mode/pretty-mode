c0555111eea48e5b05baf0f4e56c9c981375ddb3
Grant Rettke <gcr@stoneisle.com>
2013-06-08 21:47:55 -0500

    Added sections in preparation for covering all along with fixes.

--- fixtures/fixture.scm
+++ fixtures/fixture.scm
@@ -2,23 +2,31 @@
 
 (import (rnrs base (6)))
 
+;;; Ordering
 ;;; Equality
-
 (assert '!=) ; 2260 ≠ NOT EQUAL TO
-
 ;;; Logic
-
 (assert 'not) ; 00AC ¬ NOT SIGN
 (assert 'and) ; 2227 ∧ LOGICAL AND
 (assert 'or) ; 2228 ∨ LOGICAL OR
-
+;;; Sets
+;;; Subscripts and Superscripts
+;;; Function
+;;; Greek Letters
+;;; Punctuation
+;;; Types
 ;;; Arrows
-
 (assert '<-) ; 2190 ← LEFTWARDS ARROW
 (assert '->) ; 2192 → RIGHTWARDS ARROW
 (assert '->>) ; 21A0 ↠ RIGHTWARDS TWO HEADED ARROW
 (assert '=>) ; 21D2 ⇒ RIGHTWARDS DOUBLE ARROW
 (assert '...) ; 2026 … HORIZONTAL ELLIPSIS
+;;; Quantifiers
+;;; Nil
 (assert 'null) ; 2205 ∅ EMPTY SET "null"
 (assert ''()) ; 2205 ∅ EMPTY SET "'()"
 (assert 'empty) ; 2205 ∅ EMPTY SET  "empty"
+;;; Arithmetic
+;;; Undefined
+;;; Parentheses
+;;; Other

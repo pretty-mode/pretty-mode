15723e66a79e685ca3dd53446b57ef1d160cbdda
Grant Rettke <gcr@stoneisle.com>
2013-06-08 22:10:29 -0500

    Added entries for greek caps

--- fixtures/fixture.scm
+++ fixtures/fixture.scm
@@ -3,6 +3,8 @@
 (import (rnrs base (6)))
 
 ;;; Ordering
+(assert '<=) ; 2264 ≤ LESS-THAN OR EQUAL TO
+(assert '>=) ; 2265 ≥ GREATER-THAN OR EQUAL TO
 ;;; Equality
 (assert '!=) ; 2260 ≠ NOT EQUAL TO
 ;;; Logic
@@ -14,6 +16,54 @@
 ;;; Function
 (assert 'lambda) ; 03BB λ GREEK SMALL LETTER LAMDA
 ;;; Greek Letters
+(assert 'ALPHA) ; 0391 Α GREEK CAPITAL LETTER ALPHA
+(assert 'Alpha) 
+(assert 'BETA) ; 0392 Β GREEK CAPITAL LETTER BETA
+(assert 'Beta) 
+(assert 'GAMMA) ; 0393 Γ GREEK CAPITAL LETTER GAMMA
+(assert 'Gamma)
+(assert 'DELTA) ; 0394 Δ GREEK CAPITAL LETTER DELTA
+(assert 'Delta) 
+(assert 'EPSILON) ; 0395 Ε GREEK CAPITAL LETTER EPSILON
+(assert 'Epsilon) 
+(assert 'ZETA) ; 0396 Ζ GREEK CAPITAL LETTER ZETA
+(assert 'Zeta)
+(assert 'ETA) ; 0397 Η GREEK CAPITAL LETTER ETA
+(assert 'Eta)
+(assert 'THETA) ; 0398 Θ GREEK CAPITAL LETTER THETA
+(assert 'Theta)
+(assert 'IOTA) ; 0399 Ι GREEK CAPITAL LETTER IOTA
+(assert 'Iota)
+(assert 'KAPPA) ; 039A Κ GREEK CAPITAL LETTER KAPPA
+(assert 'Kappa)
+(assert 'LAMBDA) ; 039B Λ GREEK CAPITAL LETTER LAMBDA
+(assert 'Lambda)
+(assert 'MU) ; 039C Μ GREEK CAPITAL LETTER MU
+(assert 'Mu)
+(assert 'NU) ; 039D Ν GREEK CAPITAL LETTER NU
+(assert 'Nu)
+(assert 'XI) ; 039E Ξ GREEK CAPITAL LETTER XI
+(assert 'Xi)
+(assert 'OMICRON) ; 039F Ο GREEK CAPITAL LETTER OMICRON
+(assert 'Omicron)
+(assert 'PI) ; 03A0 Π GREEK CAPITAL LETTER PI
+(assert 'Pi)
+(assert 'RHO) ; 03A1 Ρ GREEK CAPITAL LETTER RHO
+(assert 'Rho)
+(assert 'SIGMA) ; 03A3 Σ GREEK CAPITAL LETTER SIGMA
+(assert 'Sigma)
+(assert 'TAU) ; 03A4 Τ GREEK CAPITAL LETTER TAU
+(assert 'Tau)
+(assert 'UPSILON) ; 03A5 Υ GREEK CAPITAL LETTER UPSILON
+(assert 'Upsilon)
+(assert 'PHI) ; 03A6 Φ GREEK CAPITAL LETTER PHI
+(assert 'Phi)
+(assert 'CHI) ; 03A7 Χ GREEK CAPITAL LETTER CHI
+(assert 'Chi)
+(assert 'PSI) ; 03A8 Ψ GREEK CAPITAL LETTER PSI
+(assert 'Psi)
+(assert 'OMEGA) ; 03A9 Ω GREEK CAPITAL LETTER OMEGA
+(assert 'Omega)
 ;;; Punctuation
 ;;; Types
 ;;; Arrows

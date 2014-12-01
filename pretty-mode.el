4ba8fceb7dd733361ed975d80ac2caa3612fa78b
Grant Rettke <grettke@acm.org>
2014-12-01 13:45:19 -0600

    Incremented the release for tagging
    R/ESS mode additions.

--- pretty-mode.el
+++ pretty-mode.el
@@ -11,7 +11,7 @@
 ;; Maintainer: Dmitri Akatov <akatov@gmail.com>
 ;; URL: https://github.com/akatov/pretty-mode
 ;; Keywords: pretty, unicode, symbols
-;; Version: 2.0.2
+;; Version: 2.0.3
 
 ;; This file is NOT part of GNU Emacs.
 
